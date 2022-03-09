2d0535a1960a5766787f2c1e7c9c679697e7c62d
adamnussbaum1 <adam.nussbaum@kellogg.northwestern.edu>
2022-03-09 18:13:48 +0000

    authentication & authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,26 @@ class SessionsController < ApplicationController
 
   def create
     # authenticate the user
-    # @user = User.where({ email: params["email"] })[0]
     @user = User.find_by({ email: params["email"] })
     if @user
       if BCrypt::Password.new(@user.password) == params["password"]
         session[:user_id] = @user.id
-        flash[:notice] = "Welcome, #{@user.first_name}."
+        flash[:notice] = "Welcome"
         redirect_to "/places"
       else
-        flash[:notice] = "Nope"
+        flash[:notice] = "Password or Username is incorrect. Please try Again!"
         redirect_to "/sessions/new"
       end
     else
-      flash[:notice] = "Nope"
+      flash[:notice] = "Password or Username is incorrect. Please try Again!"
       redirect_to "/sessions/new"
     end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You have been logged out"
+    redirect_to "/sessions/new"
   end
 end
   
