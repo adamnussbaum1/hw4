49270b8475d65b4dbb2a9ecb2aeb27debef6e7a7
adamnussbaum1 <adam.nussbaum@kellogg.northwestern.edu>
2022-03-09 16:50:10 +0000

    sync

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(params["user"]["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/users/#{@user.id}"
   end
 end
+
+def show
+  @user = User.find(params["id"])
+end
+
+
